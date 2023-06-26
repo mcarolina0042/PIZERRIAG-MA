e7d1c86864f4d104f067e41f6b4b6a45949b1b30
Maria Carolina <HP User@DESKTOP-EQ2VM2G>
2023-06-26 07:48:02 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using PIZZASG_M.Data;
+using PIZZASG_M.Data.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<MyDBContext>();
+builder.Services.AddScoped<IMyDBContext, MyDBContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
